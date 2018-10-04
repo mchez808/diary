5e9a4833d03de23ce70fcd6854a3e884ea8e4ed2
Mark Chesney <mark.chesney@gmail.com>
2018-10-04 15:59:09 -0700

    tabs replaced with spaces

--- diary.py
+++ diary.py
@@ -8,63 +8,72 @@ db = SqliteDatabase('diary.db')
 
 
 class Entry(Model):
-    content = TextField()
-    timestamp = DateTimeField(default=datetime.datetime.now)
+	content = TextField()
+	timestamp = DateTimeField(default=datetime.datetime.now)
 
-    class Meta:
-        database = db
+	class Meta:
+		database = db
 
 
 def initialize():
-    """Create the database and table if they don't exist."""
-    db.connect()
-    db.create_tables([Entry], safe=True)
+	"""Create the database and table if they don't exist."""
+	db.connect()
+	db.create_tables([Entry], safe=True)
 
 
 def menu_loop():
-    """Show the menu."""
-    choice = None
+	"""Show the menu."""
+	choice = None
 
-    while choice != 'q':
-        print("Enter 'q' to quit.")
-        for key, value in menu.items():
-            print("{}) {} ".format(key, value.__doc__))
-        choice = input('Action: ').lower().strip()
+	while choice != 'q':
+		print("Enter 'q' to quit.")
+		for key, value in menu.items():
+			print("{}) {} ".format(key, value.__doc__))
+		choice = input('Action: ').lower().strip()
 
-        if choice in menu:
-            menu[choice]()
+		if choice in menu:
+			menu[choice]()
 
 
 def add_entry():
-    """Add an entry."""
+	"""Add an entry."""
 
 
-def view_entries():
-    """View all entries."""
+def view_entries(search_query=None):
+	"""View all entries."""
 	entries = Entry.select().order_by(Entry.timestamp.desc())
-	
+	if search_query:
+		entries = entries.where(Entry.content.contains(search_query))
+
 	for entry in entries:
 		timestamp = entry.timestamp("%A %B %d, %Y %I:%M%p")
 		print(timestamp)
 		print('='*len(timestamp))
 		print(entry.content)
-		print('n) next entry')
-		print('q)' return to main menu')
+		print("n) next entry")
+		print("q)' return to main menu")
 		
 		next_action = input('Action: [Nq] '.lower().strip())
 		if next_action == 'q':
 			break
 
 
+def search_entries():
+	"""Search entries for a string."""
+	view_entries(input('Search query: '))
+
+
 def delete_entry(entry):
-    """Delete this entry."""
+	"""Delete this entry."""
 
 
 menu = OrderedDict([
-    ('a', add_entry),
-    ('v', view_entries),
+	('a', add_entry),
+	('v', view_entries),
+	('s', search_entries),
 ])
 
+
 if __name__ == '__main__':
-    initialize()
-    menu_loop()
+	initialize()
+	menu_loop()
